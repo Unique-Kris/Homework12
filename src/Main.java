44138fa07ce8875f1a11b57733100c2fe6b90ac3
Kristina Pravdina <titina91@mail.ru>
2024-07-07 08:07:01 +0300

    Revert "HW12"

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        Author firstAuthor = new Author("Nikolai", "Prokhorenok");
-        Author secondAuthor = new Author("Herbert", "Schildt");
 
-        Book firstBook = new Book("Basics Java", firstAuthor, 2019);
-        firstBook.setYearOfPublication(2022);
-        Book secondBook = new Book("Java. The Complete Guide", secondAuthor, 2022);
-        secondBook.setYearOfPublication(2023);
     }
 }
