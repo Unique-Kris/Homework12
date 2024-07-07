5bc16a1a14066d968776ab78919f344812e8afee
Unique-Kris <titina91@mail.ru>
2024-07-07 09:15:32 +0300

    HW12

--- src/Book.java
+++ src/Book.java
@@ -1,7 +1,31 @@
+import java.util.Objects;
+
 public class Book {
     private String nameBook;
     private Author author;
     private int yearOfPublication;
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "nameBook='" + nameBook + '\'' +
+                ", author=" + author +
+                ", yearOfPublication=" + yearOfPublication +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return yearOfPublication == book.yearOfPublication && Objects.equals(nameBook, book.nameBook) && Objects.equals(author, book.author);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nameBook, author, yearOfPublication);
+    }
 
     public Book(String nameBook, Author author, int yearOfPublication) {
         this.nameBook = nameBook;
--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,18 @@
 public class Main {
+
     public static void main(String[] args) {
         Author firstAuthor = new Author("Nikolai", "Prokhorenok");
         Author secondAuthor = new Author("Herbert", "Schildt");
+        System.out.println("firstAuthor = " + firstAuthor);
+        System.out.println("secondAuthor = " + secondAuthor);
 
         Book firstBook = new Book("Basics Java", firstAuthor, 2019);
         firstBook.setYearOfPublication(2022);
         Book secondBook = new Book("Java. The Complete Guide", secondAuthor, 2022);
         secondBook.setYearOfPublication(2023);
+        System.out.println("firstBook = " + firstBook);
+        System.out.println("secondBook = " + secondBook);
+
+
     }
 }
